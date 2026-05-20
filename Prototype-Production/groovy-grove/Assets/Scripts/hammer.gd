45cab00466df314ffa8a7d7b1f5b35b008dfcee9
JA-P07 <julius.palttala@gmail.com>
2026-05-20 14:37:19 +0300

    items added

---
+++ Prototype-Production/groovy-grove/Assets/Scripts/hammer.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var interaction_area = $InteractableArea
+
+@export var item_name = "hammer"
+@export var item_instance: Node2D
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact(player):
+
+	self.add_to_group("hammer");
+	player.pickup_item(item_name, self)
+	print("HOLDING:", player.is_holding)
+	print("ITEM:", player.held_item_name)
